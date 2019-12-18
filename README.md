36f1ee2a38bc479da00aec4fba69e382e688b69d
Alejandro Barrios <alejandro.barrios87@hotmail.com>
2019-12-18 09:36:22 -0300

    New new pull

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Final_pp3
+--
+Comandos utiles
+- docker run -it --rm --entrypoint sh python:3-alpine
+Para ingresar al phyton y probar buideo a mano
+
+ - docker-compose build --force-rm --pull --no-cache
+Forzado de buildeo de version
--- app/api.py
+++ app/api.py
@@ -1,21 +1,19 @@
+from time import ctime
 from flask import Flask
-from flask.ext.spyne import Spyne
-from spyne.protocol.soap import Soap11
-from spyne.model.primitive import Unicode, Integer
-from spyne.model.complex import Iterable
+from flaskext.enterprise import Enterprise
 
 app = Flask(__name__)
-spyne = Spyne(app)
 
-class SomeSoapService(spyne.Service):
-    __service_url_path__ = '/soap/someservice'
-    __in_protocol__ = Soap11(validator='lxml')
-    __out_protocol__ = Soap11()
+enterprise = Enterprise(app)
 
-    @spyne.srpc(Unicode, Integer, _returns=Iterable(Unicode))
-    def echo(str, cnt):
-        for i in range(cnt):
-            yield str
+
+class DemoService(enterprise.SOAPService):
+    __soap_server_address__ = '/soap'
+    __soap_target_namespace__ = 'ns'
+
+    @enterprise.soap(_returns=enterprise._sp.String)
+    def get_time(self):
+        return ctime()
 
 if __name__ == '__main__':
-    app.run(host = '127.0.0.1')
+    app.run(host='127.0.0.1', port=5555)
